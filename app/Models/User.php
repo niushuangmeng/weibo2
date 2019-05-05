2bf649d63d93175e56f6719f38ce69f4c8b5051b
vagrant <vagrant@homestead>
2019-05-05 08:17:54 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable;//消息通知相关功能引用
 
     /**
      * The attributes that are mass assignable.
