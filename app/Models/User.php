517a58eaabfabfc37508d809bf8612b2fb2a7edd
Sam <niushuangmeng@gmail.com>
2019-05-31 15:10:46 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Str;
-
+use Auth;
 class User extends Authenticatable
 {
     use Notifiable;//消息通知相关功能引用
@@ -57,11 +57,11 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
-    public function feed()
-    {
-        return $this->statuses()
-                    ->orderBy('created_at','desc');
-    }
+    // public function feed()
+    // {
+    //     return $this->statuses()
+    //                 ->orderBy('created_at','desc');
+    // }
 
     public function followers()
     {
@@ -93,6 +93,15 @@ class User extends Authenticatable
     {
         return $this->followings->contains($user_id);
     }
+
+    public function feed()
+   {
+       $user_ids = $this->followings->pluck('id')->toArray();
+       array_push($user_ids, $this->id);
+       return Status::whereIn('user_id', $user_ids)
+                             ->with('user')
+                             ->orderBy('created_at', 'desc');
+   }
 
 
 }
